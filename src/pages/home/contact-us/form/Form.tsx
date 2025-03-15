e9cc7d9838a375475385e97ff287cfa0d6b4d88f
rmere <rmere@griddynamics.com>
2025-03-15 00:01:07 -0600

    Add contact us form

---
+++ src/pages/home/contact-us/form/Form.tsx
@@ -0,0 +1,56 @@
+import TextInput from '../../../../components/input/TextInput';
+import { checkBoxOptions, radioOptions } from './multipleOptions';
+import MultipleOptionInput from '../../../../components/input/MultipleOptionInput';
+import Button from '../../../../components/input/Button';
+
+export default function ContactUsForm() {
+  return (
+    <form className="flex flex-2/3 flex-col gap-24 px-20 py-48">
+      <div className="grid grid-cols-2 gap-20">
+        <TextInput
+          id="first-name"
+          label="First Name"
+          type="text"
+          placeholder="John"
+        />
+        <TextInput
+          id="last-name"
+          label="Last Name"
+          type="text"
+          placeholder="Doe"
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-20">
+        <TextInput
+          id="email"
+          label="Email"
+          type="email"
+          placeholder="example@gmail.com"
+        />
+        <TextInput
+          id="phone-number"
+          label="Phone Number"
+          type="tel"
+          placeholder="00 000 00 00"
+        />
+      </div>
+      <MultipleOptionInput
+        type="radio"
+        options={radioOptions}
+        name="position"
+      />
+      <TextInput
+        id="message"
+        label="Message"
+        type="textarea"
+        placeholder="Text here..."
+      />
+      <MultipleOptionInput
+        type="checkbox"
+        options={checkBoxOptions}
+        name="terms"
+      />
+      <Button Content="Send Message" className="mt-12 self-end" />
+    </form>
+  );
+}
