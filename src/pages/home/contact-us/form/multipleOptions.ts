c691f06117acbebb34e5d058d357cc625db99da6
rmere <rmere@griddynamics.com>
2025-03-17 13:41:43 -0600

    Change multiple option input to accept required prop

--- src/pages/home/contact-us/form/multipleOptions.ts
+++ src/pages/home/contact-us/form/multipleOptions.ts
@@ -2,11 +2,22 @@ export interface MultipleOptionData {
   id: string;
   label: string;
   value: string;
+  required?: boolean;
 }
 
 export const checkBoxOptions: MultipleOptionData[] = [
-  { id: 'terms-1', label: 'Accept the Terms', value: 'terms-1' },
-  { id: 'terms-2', label: 'Accept the Terms 2', value: 'terms-2' },
+  {
+    id: 'terms-1',
+    label: 'Accept the Terms',
+    value: 'terms-1',
+    required: true,
+  },
+  {
+    id: 'terms-2',
+    label: 'Accept the Terms 2',
+    value: 'terms-2',
+    required: true,
+  },
 ];
 
 export const radioOptions: MultipleOptionData[] = [
