cfb4ed647783a6fc4b686418a92a117212f81313
rmere <rmere@griddynamics.com>
2025-03-18 13:53:01 -0600

    Add discernable names to links

--- src/pages/home/about-us/AboutUsCard.tsx
+++ src/pages/home/about-us/AboutUsCard.tsx
@@ -21,7 +21,11 @@ export default function AboutUsCard({
       <div className="ml-16 flex-2/3 text-white">
         <h2 className="text-large mb-12 font-bold">{title}</h2>
         <p className="mb-24 grow text-base">{description}</p>
-        <a href="/" className="text-base underline">
+        <a
+          href="/"
+          className="text-base underline"
+          aria-label={`Learn more about ${title}`}
+        >
           Learn more about →
         </a>
       </div>
