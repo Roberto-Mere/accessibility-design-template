e43ffaf605a4a1ec6711f981802ebddc29360b58
rmere <rmere@griddynamics.com>
2025-03-18 21:41:52 -0600

    Fix contact us side image size on smaller screens

--- src/pages/home/contact-us/side/Side.tsx
+++ src/pages/home/contact-us/side/Side.tsx
@@ -3,9 +3,9 @@ import Socials from './Socials';
 
 export default function Side() {
   return (
-    <section className="side-background flex-1/3 rounded-2xl p-48">
+    <section className="side-background flex flex-1/3 flex-col rounded-2xl p-48">
       <h2 className="mb-16 text-3xl font-bold">Contact Us</h2>
-      <p className="mb-56 text-base">
+      <p className="mb-56 text-base max-md:mb-32">
         Fill up the form and ur Team will get back to you within 24 hours. Happy
         to see your message!
       </p>
