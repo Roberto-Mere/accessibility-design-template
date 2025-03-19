a719320872e0db13aac09a7d5458f03b9dc0e617
rmere <rmere@griddynamics.com>
2025-03-18 21:07:23 -0600

    Add contact us responsive styles

--- src/pages/home/contact-us/ContactUs.tsx
+++ src/pages/home/contact-us/ContactUs.tsx
@@ -1,12 +1,12 @@
-import ContactUsForm from './form/Form';
+import Form from './form/Form';
 import Side from './side/Side';
 
 export default function ContactUs() {
   return (
-    <section className="bg-light-green flex px-64 pt-80 pb-[12rem]">
-      <div className="flex grow rounded-2xl bg-white">
+    <section className="bg-light-green max-xs:px-16 max-xs:pt-20 max-xs:pb-32 flex px-64 pt-80 pb-[12rem] max-sm:px-32 max-sm:pt-40 max-sm:pb-60">
+      <div className="flex grow flex-wrap rounded-2xl bg-white">
         <Side />
-        <ContactUsForm />
+        <Form />
       </div>
     </section>
   );
