210325681746e4eaa53e8943d23d6265de515fce
rmere <rmere@griddynamics.com>
2025-03-18 21:57:01 -0600

    Fix search bar button accessible name

--- src/layout/header/SearchBar.tsx
+++ src/layout/header/SearchBar.tsx
@@ -5,11 +5,12 @@ export default function SearchBar() {
   return (
     <div className="max-xs:ml-12 ml-36 flex grow justify-end">
       <input
+        id="search"
         type="search"
         placeholder="Search this site"
         className="placeholder:text-dark-green-faded text-large border-dark-green leading-large max-xs:max-w-md max-w-xl grow border-2 px-16 py-8"
       />
-      <Button Content={() => <SearchIcon />} />
+      <Button Content={() => <SearchIcon />} ariaLabel="Search" />
     </div>
   );
 }
